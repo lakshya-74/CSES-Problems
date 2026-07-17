5c15d025d04c9a9f3564b34fc3523e6f38dfe527
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-07-17 16:19:11 +0530

    counting towers

--- dp/Counting_Towers.cpp
+++ dp/Counting_Towers.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 #define ll long long
 #define pp pair<int,int>
-
-vector<vector<int>> dp(1e6+1,vector<int>(2));
+int mod = 1e9 +7;
+vector<vector<ll>> dp(1e6+1,vector<ll>(2));
+void pre(){
+    dp[1][0]   = 1;
+    dp[1][1]   = 1;
+    for(int i=2;i<=1e6;i++){
+        dp[i][0] = (4*dp[i-1][0] + dp[i-1][1])%mod;
+        dp[i][1] = (dp[i-1][0] + 2*dp[i-1][1])%mod;
+    }
+}
 void solve(){
     int n;
     cin>>n;
-    dp[n][0]= 1;
-    dp[n][1] = 1;
-    int mod = 1e9 +7;
-    for(int i=n-2;i>=0;i--){
-        dp[i][0] = (2LL*dp[i+1][0] + dp[i+1][1])%mod;
-        dp[i][1] = (4LL*dp[i+1][1] + dp[i+1][0])%mod;
-    }
-    cout<<(dp[1][0] + dp[1][1])%mod<<endl;
+    
+    cout<<(dp[n][0]+dp[n][1])%mod<<endl;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    pre();
    int t;
  cin>>t;
 while(t--){
